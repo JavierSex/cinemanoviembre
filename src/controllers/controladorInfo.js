b076146f7ce96638c2b0e86b0c083ec148e70f8e
jablancohe@cesde.net <jablancohe@cesde.net>
2022-11-21 16:39:21 -0500

    actualizacion y adjuntar todo

---
+++ src/controllers/controladorInfo.js
@@ -0,0 +1,16 @@
+//vamos a pintar los datos obtenidos en memoria
+let peliculaConsultadaEnMemoria=JSON.parse(localStorage.getItem("pelicula"))
+console.log(peliculaConsultadaEnMemoria)
+
+//asignando valores a las etiquetas que quiero controlar:
+let etiquetaImagen=document.getElementById("imagenPelicula")
+etiquetaImagen.src=peliculaConsultadaEnMemoria.imagen
+
+let etiquetaTitulo=document.getElementById("nombrePelicula")
+etiquetaTitulo.textContent=peliculaConsultadaEnMemoria.titulo
+
+let etiquetaDuracion=document.getElementById("duracionPelicula")
+etiquetaDuracion.textContent=peliculaConsultadaEnMemoria.duracion
+
+let etiquetaSinopsis=document.getElementById("sinopsisPelicula")
+etiquetaSinopsis.textContent=peliculaConsultadaEnMemoria.sinopsis
--- src/controllers/listadoPeliculas.js
+++ src/controllers/listadoPeliculas.js
@@ -2,53 +2,63 @@ let peliculas=[
     {
         nombre:"Avatar 2",
         duracion:120,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemajavier-f9274.appspot.com/o/avatar2Pre.png?alt=media&token=3126a80b-64f6-4cbd-9106-39545726e32f"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemajavier-f9274.appspot.com/o/avatar2Pre.png?alt=media&token=3126a80b-64f6-4cbd-9106-39545726e32f",
+        sinopsis:"ake Sully y Ney'tiri han formado una familia y hacen todo lo posible por permanecer juntos. Sin embargo, deben abandonar su hogar y explorar las regiones de Pandora cuando una antigua amenaza reaparece."
 
     },
     {
         nombre:"El llamado del anticristo",
         duracion:90,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemajavier-f9274.appspot.com/o/elLlamadoDelAnticristoPre.png?alt=media&token=df45862a-6f7f-4b58-82fc-efc00e2ae8f4"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemajavier-f9274.appspot.com/o/elLlamadoDelAnticristoPre.png?alt=media&token=df45862a-6f7f-4b58-82fc-efc00e2ae8f4",
+        sinopsis:"Traducción del inglés-The Possessed es una película de terror australiana de 2021 escrita y dirigida por Chris Sun y protagonizada por John Jarratt, Lincoln Lewis y Angie Kent. La película se estrenó en el Festival de Cine FrightFest de Londres de 2021 y estaba programada para estrenarse en Australia en noviembre de 2021."
     },
     {
         nombre:"El Menú",
         duracion:85,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemajavier-f9274.appspot.com/o/elMenuPre.png?alt=media&token=b78170fc-e0bb-4ae4-b50b-aebee376bd34"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemajavier-f9274.appspot.com/o/elMenuPre.png?alt=media&token=b78170fc-e0bb-4ae4-b50b-aebee376bd34",
+        sinopsis:"The Menu es una película estadounidense de suspenso y comedia negra de 2022 dirigida por Mark Mylod, escrita por Seth Reiss y Will Tracy, y producida por Adam McKay, Betsy Koch y Will Ferrell."
     },
     {
         nombre:"Gato con Botas",
         duracion:104,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemajavier-f9274.appspot.com/o/gatoConBotasPre.png?alt=media&token=262c41a9-cb8b-441c-b863-c17c264daed0"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemajavier-f9274.appspot.com/o/gatoConBotasPre.png?alt=media&token=262c41a9-cb8b-441c-b863-c17c264daed0",
+        sinopsis:"El Gato con Botas: el último deseo es una próxima película estadounidense de comedia de aventuras animada por computadora producida por DreamWorks Animation y distribuida por Universal Pictures. La película es una secuela de El Gato con Botas y es derivada de la franquicia de Shrek."
     },
     {
         nombre: "La gran tormenta",
         duracion:95,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemajavier-f9274.appspot.com/o/laGranTormentaPre.jpg?alt=media&token=b5bd60ef-0d4e-4411-94e5-be6c22fb8a56"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemajavier-f9274.appspot.com/o/laGranTormentaPre.jpg?alt=media&token=b5bd60ef-0d4e-4411-94e5-be6c22fb8a56",
+        sinopsis:"Cuando su avión se estrella en una remota montaña cubierta de nieve, Jane y Paul tienen que luchar por sus vidas como los únicos sobrevivientes que quedan. Juntos se embarcan en un angustioso viaje fuera del desierto."
     },
     {
         nombre: "La llorona",
         duracion:95,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemajavier-f9274.appspot.com/o/laLloronaPre.png?alt=media&token=5612fdb1-6a2f-4f2d-b4e0-b2d3997a1f20"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemajavier-f9274.appspot.com/o/laLloronaPre.png?alt=media&token=5612fdb1-6a2f-4f2d-b4e0-b2d3997a1f20",
+        sinopsis:"Andrew y Carly una joven pareja, viajan junto su hijo Danny a una remota hacienda en las afueras de la ciudad de Mexico para unas merecidas vacaciones, sin imaginar que serían victimas de espíritus malignos y eventos sobrenaturales que desencadenarían la desaparición de su pequeño hijo. Tras fallidas búsquedas la pareja descubre que la desaparición de Danny está ligada a la maldición de La Llorona, mito mexicano donde una mujer pierde a su hijo y su alma en pena no descansará hasta cobrar venganza"
     },
     {
         nombre: "Lilo, Lilo, Cocodrilo",
         duracion:95,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemajavier-f9274.appspot.com/o/lyleCocodriloPre.jpg?alt=media&token=099972e6-29fc-46e9-94b5-ebfa6c519fa8"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemajavier-f9274.appspot.com/o/lyleCocodriloPre.jpg?alt=media&token=099972e6-29fc-46e9-94b5-ebfa6c519fa8",
+        sinopsis:"Basado en la serie best selling de libros con el mismo nombre, Lyle, Lyle Crocodile es traida a la gran pantalla como un musical live action para toda la familia. La historia sigue a la familia Primm, que acaba de llegar a Nueva York por el nuevo trabajo del Señor Primm. Josh Primm, de 12 años, le teme a muchas cosas y le cuesta hacer amigos. Cuando Josh descubre a Lyle, un cocodrilo que baila y canta en su ático, los dos forman un vínculo inusual mientras Lyle le muestra la magia de Nueva York a su nuevo amigo. Mientras cada miembro de la familia encuentra una relación única con Lyle, los Primm se transforman y adoptan un nuevo sentido del amor y la aventura. Cuando el antiguo dueño de Lyle, el excéntrico Hector P. Valenti, vuelve a reclamar a su cocodrilo para un nuevo acto musical, los Primm se dan cuenta que este cocodrilo super talentoso y con un gran corazón les cambió la vida para siempre y deben ir a luchar para mantenerlo en la familia. ¡Con música original de Pasek & Paul (La La Land, El Gran Showman), el cocodrilo cantante nos tendrá a todos cantando con él!        "
     },
     {
         nombre: "Mar de sangre",
         duracion:95,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemajavier-f9274.appspot.com/o/marDeSangrePre.png?alt=media&token=03631042-8442-4efd-90d5-6e158a45300e"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemajavier-f9274.appspot.com/o/marDeSangrePre.png?alt=media&token=03631042-8442-4efd-90d5-6e158a45300e",
+        sinopsis:"Un grupo de amigos roban un par de motos acuáticas pero terminan en un terrible accidente. En el mar y con un herido, tratarán de encontrar el camino de regreso a casa y de salvarse de la amenaza de los depredadores más temidos del océano.        "
     },
     {
         nombre: "One Piece Film Red",
         duracion:95,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemajavier-f9274.appspot.com/o/onePieceRedPre.png?alt=media&token=12ed362c-d4f5-4e26-97a4-f4f3e7eb4224"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemajavier-f9274.appspot.com/o/onePieceRedPre.png?alt=media&token=12ed362c-d4f5-4e26-97a4-f4f3e7eb4224",
+        sinopsis:"Uta, la cantante más popular del mundo, va a presentarse en un escenario y revelar su apariencia por primera vez. Luffy y sus amigos acuden al concierto y se dan cuenta de que la voz de Uta es capaz de cambiarlo todo."
     },
     {
         nombre: "Rebelión",
         duracion:95,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemajavier-f9274.appspot.com/o/rebelionPre.png?alt=media&token=7e4cea25-f435-4fea-a3f7-76c0263e8e2a"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemajavier-f9274.appspot.com/o/rebelionPre.png?alt=media&token=7e4cea25-f435-4fea-a3f7-76c0263e8e2a",
+        sinopsis:"La película retrata a un vulnerable y anónimo Joe Arroyo, quizás el cantante colombiano de salsa más importante de la historia, en su viaje claustrofóbico por diferentes momentos de su vida. Un genio musical, cantante y compositor, la música y la voz única de Joe no se pudieron contener en el escenario. La película sigue al hombre hasta sus profundidades más íntimas al compás del alma de su amada, atormentada por su gran amor: la música. Es una exploración atemporal de su vida, una aventura en el lugar oscuro y solitario donde el talento de Joe estaba en su punto más crudo y sus emociones e impulsos eran libres. La historia tiene lugar en habitaciones de hotel en diferentes ciudades plagadas del caos de Joe. Todos podrían estar en la misma habitación y apenas contener la agonía de Joe al ver cómo sabotea todo lo que ama.        "
     }
 ]
 
@@ -91,11 +101,41 @@ peliculas.forEach(function(pelicula){
     duracion.classList.add("text-center")
     duracion.textContent="duracion: " + pelicula.duracion + "min"
 
+    //traversing
+    let sinopsis=document.createElement("p")
+    sinopsis.classList.add("d-none")
+    sinopsis.textContent=pelicula.sinopsis
+
     //PADRES E HIJOS
     etiquetaFila.appendChild(columna)
     columna.appendChild(tarjeta)
     tarjeta.appendChild(poster)
     tarjeta.appendChild(nombre)
     tarjeta.appendChild(duracion)
+    tarjeta.appendChild(sinopsis)
 
+})
+
+
+//Escuchando el evento click en alguna de las carteleras de las peliculas
+let peliculaSeleccionada={}   //objeto{}
+etiquetaFila.addEventListener("click",function(evento){
+
+    if (evento.target.classList.contains("img-fluid")==true) {
+        
+        //obteniendo los datos de la pelicula
+        peliculaSeleccionada.imagen=evento.target.parentElement.querySelector("img").src
+        peliculaSeleccionada.titulo=evento.target.parentElement.querySelector("h3").textContent
+        peliculaSeleccionada.duracion=evento.target.parentElement.querySelector("h5").textContent
+        peliculaSeleccionada.sinopsis=evento.target.parentElement.querySelector("p").textContent
+
+        //enviando nuestros datos de la pelicula seleccionada a la memoria
+
+        localStorage.setItem("pelicula",JSON.stringify(peliculaSeleccionada))
+
+        window.location.href="/src/views/verinfopelicula.html"
+        //cookies es un concepto para guardar datos de
+        //forma local para mandarlo a otra parte y pintarlos
+        //el remplazo de las cookies se llaman session storage local
+    }    
 })
